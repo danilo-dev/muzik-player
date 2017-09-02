cce83bf4aaa2b349e12bd6901632130181a6b93e
Danilo de Oliveira <danilo.desenvolvedor@outlook.com>
2017-09-02 19:28:32 -0300

    Exibição do ícone para acesso à fila de reprodução (ainda não implementado)

--- MuzikPlayer/app/src/main/java/br/com/danilooliveira/muzikplayer/activities/PlayerActivity.java
+++ MuzikPlayer/app/src/main/java/br/com/danilooliveira/muzikplayer/activities/PlayerActivity.java
@@ -9,6 +9,9 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.SeekBar;
@@ -40,6 +43,12 @@ public class PlayerActivity extends BaseActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
+
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setTitle(null);
+        }
 
         mViewPager = (ViewPager) findViewById(R.id.view_pager);
         txtCurrentDuration = (TextView) findViewById(R.id.txt_current_duration);
@@ -114,6 +123,25 @@ public class PlayerActivity extends BaseActivity {
                 });
             }
         }, 0, 17);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.activity_player, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_queue:
+                break;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
     }
 
     @Override
